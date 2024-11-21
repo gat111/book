168cdabf62826929a8f03b525282a354279576b8
kazu2377 <jtths474@gmail.com>
2024-11-21 20:28:48 +0900

    修正

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,27 +1,15 @@
 package dao;
 
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
 public class DAO {
-    private static String JDBC_URL;
-    private static String DB_USER;
-    private static String DB_PASSWORD;
 
-    static {
-        try (InputStream input = DAO.class.getClassLoader().getResourceAsStream("db.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
 
-            JDBC_URL = prop.getProperty("jdbc.url");
-            DB_USER = prop.getProperty("jdbc.user");
-            DB_PASSWORD = prop.getProperty("jdbc.password");
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load database properties", e);
-        }
-    }
+    // 接続情報を直接埋め込む
+    private static final String JDBC_URL = "jdbc:postgresql://dpg-csvhc1rv2p9s73d18ktg-a/book_j7p0";
+    private static final String DB_USER = "book_j7p0_user";
+    private static final String DB_PASSWORD = "dwzHlPprv31xNJPljuyh7qVzh2rTE5Mk";
 
     public Connection getConnection() throws Exception {
         return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
