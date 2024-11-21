6d0f2eb786fa6dc580fe9e5b77f8c0914358d7fc
kazu2377 <jtths474@gmail.com>
2024-11-21 20:19:46 +0900

    ぽすぐれ

--- src/main/java/dao/DAO.java
+++ src/main/java/dao/DAO.java
@@ -1,17 +1,49 @@
 package dao;
 
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
+import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DAO {
-	static DataSource ds;
+    private static String JDBC_URL;
+    private static String DB_USER;
+    private static String DB_PASSWORD;
 
-	public Connection getConnection() throws Exception {
-		if (ds==null) {
-			InitialContext ic=new InitialContext();
-			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/book");
-		}
-		return ds.getConnection();
-	}
+    static {
+        try (InputStream input = DAO.class.getClassLoader().getResourceAsStream("db.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+
+            JDBC_URL = prop.getProperty("jdbc.url");
+            DB_USER = prop.getProperty("jdbc.user");
+            DB_PASSWORD = prop.getProperty("jdbc.password");
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load database properties", e);
+        }
+    }
+
+    public Connection getConnection() throws Exception {
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+    }
 }
+
+
+//package dao;
+//
+//import javax.naming.InitialContext;
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//
+//public class DAO {
+//	static DataSource ds;
+//
+//	public Connection getConnection() throws Exception {
+//		if (ds==null) {
+//			InitialContext ic=new InitialContext();
+//			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/book");
+//		}
+//		return ds.getConnection();
+//	}
+//}
+//
